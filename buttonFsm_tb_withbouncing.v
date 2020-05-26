1ad49959d6790276f91d3504f7c5ed1c7d23a26f
Polyester <anonymous_571@outlook.com>
2020-05-26 13:46:34 +0300

    add buttonFsm without bouncing

--- buttonFsm_tb_withbouncing.v
+++ buttonFsm_tb_withbouncing.v
@@ -6,7 +6,8 @@ reg clk;
 reg button;
 wire stateful_button; 
 localparam period = 2;
-integer i, j, limit, file;
+integer i, j, limit, file, rand, randindex;
+integer randlist [7:0];
 
 buttonFsm UUT(
     .clk(clk),
@@ -34,6 +35,15 @@ end
 
 initial 
 begin
+    //initialize list
+    randlist[7]=4;
+    randlist[6]=3;
+    randlist[5]=2;
+    randlist[4]=1;
+    randlist[3]=1;
+    randlist[2]=1;
+    randlist[1]=1;
+    randlist[0]=1;
 
     limit = 100;
     $timeformat(-9, 0, " ns", 20);
@@ -44,7 +54,25 @@ begin
     $display("Time %0t initial output %0d\n\n\n", $time, stateful_button);
     $fdisplay(file, "Time %0t initial output %0d\n\n\n", $time, stateful_button);
     for (i = 0; i<limit; i=i+1) begin
-        button = $random % 2;
+        randindex=$random % 8;
+        randindex = randindex < 0? -randindex: randindex;
+        rand=randlist[randindex];
+        if (rand !== 1) begin
+            $display("%0d times random bouncing", rand);
+            $fdisplay(file, "%0d times random bouncing", rand);
+        end
+        for (j = 0; j<rand; j=j+1) begin
+            button = $random % 2;
+            if (button) begin
+                $display("pressed");
+                $fdisplay(file, "pressed");
+            end else begin
+                $display("NOT-pressed");
+                $fdisplay(file, "NOT-pressed");
+            end
+            #(period/16.0);
+        end
+        
         #period;
         #period;
     end
